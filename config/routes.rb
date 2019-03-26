24d7f9d03f2c304793364726dfd6ce4b01705959
Jeremy Friedel <jeremyfriedel@yahoo.com>
2019-03-26 16:37:26 -0400

    finish views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create, :edit]
+    resource :session, only: [:create, :destroy, :show]
+  end
+end 
