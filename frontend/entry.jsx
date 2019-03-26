e26bae6db0a7af2efe17c8533ff0fc3b256b4235
Jeremy Friedel <jeremyfriedel@yahoo.com>
2019-03-26 16:50:17 -0400

    setup frontend folders

---
+++ frontend/entry.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import Root from './components/root';
+// import configureStore from './store/store';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    // ReactDOM.render(<Root store={store} />, root);
+    ReactDOM.render(<h1>Hello Internet!</h1>, root);
+});
